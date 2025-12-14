5cab124bb037dc1e39a49e976d60df8a47699368
kevinSelles <kevin.selles.c@gmail.com>
2025-12-14 05:40:38 +0100

    Update alertInterceptor.js

--- src/utils/alertInterceptor.js
+++ src/utils/alertInterceptor.js
@@ -1,9 +1,11 @@
-const originalAlert = window.alert;
+if (typeof window !== "undefined") {
+  const originalAlert = window.alert;
 
-window.alert = (message) => {
-  const event = new CustomEvent("app-alert", {
-    detail: { message },
-  });
+  window.alert = (message) => {
+    const event = new CustomEvent("app-alert", {
+      detail: { message },
+    });
 
-  window.dispatchEvent(event);
-};
+    window.dispatchEvent(event);
+  };
+}
