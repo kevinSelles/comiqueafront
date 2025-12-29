67a38eedfe63e4c8b835cc23c516362f0347d404
kevinSelles <kevin.selles.c@gmail.com>
2025-12-29 06:36:53 +0100

    añadido link a mi portfolio en el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,17 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>Comiquea — Proyecto académico realizado por Kevin Selles.</p>
+      <p>
+        Comiquea — Proyecto académico realizado por{" "}
+        <a
+          href="https://kevinfy.es/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          Kevin Selles
+        </a>.
+      </p>
     </footer>
   );
 }
